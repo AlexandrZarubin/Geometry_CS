48e7b1ccf639360368a85e5c718dee295a29fb4b
Alexander Zarubin <whitesashka@gmail.com>
2024-07-28 14:43:04 +0500

    update

--- Move/Program.cs
+++ Move/Program.cs
@@ -14,6 +14,7 @@ namespace Move
 			Random rand = new Random(); // Создание экземпляра класса Random для генерации случайных чисел
 			int width = Console.WindowWidth;// Ширина области перемещения + получение размер кончсоли
 			int height = Console.WindowHeight;// Высота области перемещения + получение размер кончсоли
+			Console.SetBufferSize(width, height);
 			int x = rand.Next(0, width);// Установка позиции курсора в координаты (x, y)
 			int y = rand.Next(0, height);
 			char BLACK_BOX = (char)9608;//Квадратик
@@ -26,6 +27,8 @@ namespace Move
 				Console.SetCursorPosition(x, y);// Установка позиции курсора в координаты (x, y)
 				Console.Write(BLACK_BOX);// Вывод символа квадратика на текущей позиции курсора
 				Console.Write(BLACK_BOX);// Вывод символа квадратика на текущей позиции курсора
+				Console.SetCursorPosition(0, Console.WindowHeight-1);
+				Console.Write($"Coordinates: ({x}, {y})");
 				ConsoleKey consolekey = Console.ReadKey(true).Key;// Считывание нажатой клавиши без отображения символа на экране
 				switch (consolekey)
 				{
